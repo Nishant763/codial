a6a427ab8040ddb34df21d68f2807985800f178e
Nishant763 <bhatianishant763@gmail.com>
2020-05-20 23:28:37 +0530

    Inital index.js route

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+//Divided the project into routes,controllers,models,config so that it is easily scalable.
+
 const express = require('express');
 const app = express();
 const port = 8000;
+
+app.use('/',require('./routes/index'));
 
 app.listen(port,function(err){
     if(err)
