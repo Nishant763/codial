62c333f8919f5d0903fc3dfd80c0a2befef45e38
Nishant763 <bhatianishant763@gmail.com>
2020-05-22 00:08:23 +0530

    Resolved one more bug in users routes

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const Router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 console.log("Routes Loaded");
+Router.use('/users',require('./users'));
+Router.use('/posts',require('./posts'));
+
 
 Router.get('/', homeController.home);
 Router.get('/aboutus', homeController.aboutUs);
-Router.get('/users/',require('./users'));
-Router.get('/posts/',require('./posts'));
 
 module.exports = Router;
--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ const Router = express.Router();
 
 const UsersController = require('../controllers/users_controller');
 
-Router.get('/users',UsersController.users);
-Router.get('/users/login',UsersController.login);
+Router.get('/',UsersController.users);
+Router.get('/login',UsersController.login);
 
 
 module.exports = Router;
