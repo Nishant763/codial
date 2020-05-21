33b4f372c328ee43c909d3ce7f4a48eb0304ff31
Nishant763 <bhatianishant763@gmail.com>
2020-05-21 16:32:16 +0530

    home-controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function(req,res){
+    return res.send('<h1>Welcome to my first Major project MEN stack</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const Router = express.Router();
-
+const homeController = require('../controllers/home_controller');
 
 console.log("Routes Loaded");
+
+Router.get('/', homeController.home);
 
 module.exports = Router;
