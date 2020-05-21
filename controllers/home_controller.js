28e9e5372858e264302f0427ae49b44d61fd06cd
Nishant763 <bhatianishant763@gmail.com>
2020-05-21 16:35:52 +0530

    added home-controller and router

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,3 @@
 module.exports.home = function(req,res){
-    return res.send('<h1>Welcome to my first Major project MEN stack</h1>');
+    return res.send('<h1>elcome to my first Major project MEN stack</h1>');
 }
