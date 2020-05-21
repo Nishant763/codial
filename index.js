9d272aad08a55f3d19e1b1f1074d23db571f4b6d
Nishant763 <bhatianishant763@gmail.com>
2020-05-22 00:30:13 +0530

    Setted up view engine (ejs) and views directory using app.set()

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.use('/',require('./routes/index'));
+//use express router
+app.use('/',require('./routes'));
 
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err)
