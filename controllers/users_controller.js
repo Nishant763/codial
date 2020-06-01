f423b92ce07a47700762e24a393adf7141e6fdb9
Nishant763 <bhatianishant763@gmail.com>
2020-06-01 23:28:13 +0530

    implemented the sign up functionality (dev)

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,32 +1,32 @@
 const user = require('../models/user');
 
 //Render the profile page
-module.exports.profile = function(req, res){
+module.exports.profile = function (req, res) {
     return res.render('user_profile', {
         title: 'User Profile'
     })
 }
 
 //Render the log in page
-module.exports.login = function(req,res){
-    return res.render('log_in',{
+module.exports.login = function (req, res) {
+    return res.render('log_in', {
         title: 'Codial || Log In'
     })
 }
 
 //post controller for log in
-module.exports.create_session = function(req,res){
+module.exports.create_session = function (req, res) {
     user.findOne({
-        email : req.body.email,
+        email: req.body.email,
         password: req.body.password
-    }, function(err,doc){
-        if(err){
+    }, function (err, doc) {
+        if (err) {
             res.send("<h3>LogIn failed!!!</h3>");
             return;
         }
-        if(doc!=null)
+        if (doc != null)
             res.send("<h3>LogIn Successfull</h3>");
-        
+
         else {
             res.send("<h3>LogIn failed!!!</h3>");
         }
@@ -34,24 +34,33 @@ module.exports.create_session = function(req,res){
 }
 
 //Render the sign up page
-module.exports.signup = function(req,res){
-    
-    return res.render('sign_up',{
-        title:'Codial || Sign Up'
+module.exports.signup = function (req, res) {
+
+    return res.render('sign_up', {
+        title: 'Codial || Sign Up'
     })
 }
 
 //Post controller for sign up
-module.exports.create = function(req,res){
+module.exports.create = function (req, res) {
     console.log(req.body);
-    if(req.body.password == req.body.confirmpassword){
-        user.create({
-            email: req.body.email,
-            password : req.body.password,
-            username : req.body.username
+    if (req.body.password == req.body.confirmpassword) {
+        user.findOne({ email: req.body.email }, function (err, doc) {
+            if (err) { console.log('error in finding user in signing up'); return; }
+            if (!doc) {
+                user.create(req.body, function (err, user) {
+
+                    if (err) { console.log('error in creating user while signing up'); return; }
+                    return res.redirect('/users/log-in');
+                })
+            }
+            else {
+                return res.redirect('back');
+            }
+
         })
-        return res.send("<h3>Successfully Signed Up!!!</h3>");
+
     }
     else
-    return res.send("<h3>Your passwords do not match</h3>");
+        return res.redirect("back");
 }
