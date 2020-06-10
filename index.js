f32dadaf69484600257669d69273e0b6190ac323
Nishant763 <bhatianishant763@gmail.com>
2020-06-10 12:40:21 +0530

    Learning to implement scss

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug:true,
+    outputStyle:'extended',
+    prefix:'/css'
+}))
 
 app.use(express.urlencoded());
 
