8f0a9adbcdb15f4cc9e3f2aa907f0b6db4ba7ab1
Nishant763 <bhatianishant763@gmail.com>
2020-06-15 17:05:42 +0530

    updating remove comment functionality

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -23,34 +23,56 @@ module.exports.create = function(req,res){
     })
     
 }
+ 
+// module.exports.destroy = function(req,res){
 
-module.exports.destroy = function(req,res){
+//     Post.findById(req.params.idpost,function(err,post){
+//         if(post){
 
-    Post.findById(req.params.idpost,function(err,post){
-        if(post){
-
-            comments.deleteOne({
-                _id:req.params.idcomment
-            },function(err){
-                if(err){console.log("Error:",err);return;}
+//             comments.deleteOne({
+//                 _id:req.params.idcomment
+//             },function(err){
+//                 if(err){console.log("Error:",err);return;}
 
                
-                post.comments.splice(post.comments.findIndex(function(i){
+//                 post.comments.splice(post.comments.findIndex(function(i){
                     
-                    return i == req.params.idcomment;
-                }),1);
-                console.log(post.comments);
-                post.save(); //don't forget this
+//                     return i == req.params.idcomment;
+//                 }),1);
+//                 console.log(post.comments);
+//                 post.save(); //don't forget this
+//                 return res.redirect('back');
+//             })
+            
+            
+//         }
+//         else{
+//             return res.redirect('back');
+//         }
+//     })
+    
+    
+
+// }
+
+//Another way of writing destroy function(Verifying whether the comment exists or not and deleting the comment)
+module.exports.destroy = function(req,res){
+    comments.findById(req.params.idcomment,function(err,comment){
+
+        if(comment.user == req.user.id){
+
+            let postId = comment.post;
+
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId,{ $pull: {comments: req.params.idcomment}},function(err,post){
+                
+                
                 return res.redirect('back');
             })
-            
-            
         }
         else{
             return res.redirect('back');
         }
     })
-    
-    
-
 }
