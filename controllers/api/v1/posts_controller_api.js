572eb6542eab36dd53336955baf6f0fd9cb4c240
Nishant763 <bhatianishant763@gmail.com>
2020-08-19 01:10:47 +0530

    deleted post using API@1 without JWT authentication

--- controllers/api/v1/posts_controller_api.js
+++ controllers/api/v1/posts_controller_api.js
@@ -1,6 +1,47 @@
-module.exports.index = function(req,res){
+const Post = require("../../../models/post");
+const Comment = require("../../../models/comment");
+module.exports.index = async function(req,res){
+    
+    let posts = await Post.find({})
+            .sort('-createdAt')
+                .populate('user','-password')
+                .populate({
+                    path:'comments',
+                    populate:{
+                        path:'user'
+                    }
+                });
+            
+    
     return res.json(200,{
-        posts: [],
+        posts: posts,
         message:  "list of posts",
     });
 }
+
+module.exports.destroy = async function(req,res){
+    try{ 
+         let post = await Post.findById(req.params.id);
+         //if(post.user == req.user.id){
+             post.remove();
+ 
+             let comments =  await Comment.deleteMany({post:req.params.id});
+ 
+           
+             
+             return res.json(200,{
+                 message:"Posts and associated comments are deleted..",
+             });
+        //  }
+        //  else{
+        //      req.flash('error','Not allowed to delete the posts');
+        //      return res.redirect('back');
+        //  }
+     }catch(err){
+        //  req.flash('error',err);
+         console.log("*****",err);
+         return res.json(500,{
+             message:"Internal Server Error",
+         });
+     }   
+ }
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -5,4 +5,6 @@ const posts_api = require('../../../controllers/api/v1/posts_controller_api');
 
 router.get('/index',posts_api.index);
 
+router.delete('/:id',posts_api.destroy);
+
 module.exports = router;
